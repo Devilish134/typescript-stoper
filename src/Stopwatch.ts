86aa04be725f2cc581b0ad2e2c095faba3887d0b
Devilish134 <kamilmusialik22@gmail.com>
2022-06-13 17:32:10 +0200

    bug repair

--- src/Stopwatch.ts
+++ src/Stopwatch.ts
@@ -47,10 +47,17 @@ abstract class Stopwatch {
     const pad0 = (num: number): string =>
       num < 10 ? `0${num}` : num.toString();
 
+    const pad0forMs = (num: number): string => {
+      const numLength = num.toString().length;
+      if (numLength === 1) return `00${num}`;
+      else if (numLength === 2) return `0${num}`;
+      else return num.toString();
+    };
+
     const mm = Math.floor(time / 60000);
     const ss = Math.floor((time - mm * 60000) / 1000);
     const ms = time - mm * 60000 - ss * 1000;
-    return `${pad0(mm)}:${pad0(ss)}:${pad0(ms).substr(0, 2)}`;
+    return `${pad0(mm)}:${pad0(ss)}:${pad0forMs(ms).substr(0, 2)}`;
   }
 
   protected renderTime(): void {
